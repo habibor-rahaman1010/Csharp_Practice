55ada22ac84250467fb7ed65dfbf1d33d0da631f
habibor-rahaman1010 <habibor144369@gmail.com>
2023-05-10 23:00:56 +0600

    Practice more and more c#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hello Programmer!");
+
+            string name1 = "Habibor";
+            string name2 = "Rahaman";
+            string fullName = name1 + " " + name2;
+            Console.WriteLine(fullName);
+
+            int x = 10;
+            int y = 20;
+            int ans = (x + y);
+            Console.WriteLine($"Summation: {ans}");
+
+            int d, g, h;
+            d = 10;
+            g = 13;
+            h = 14;
+            Console.WriteLine($"Sum is: {d + g + h}");
+        }
+    }
+}
---
+++ DataType/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            Console.WriteLine(age);
+
+            double cgpa = 3.88;
+            Console.WriteLine(cgpa);
+
+            bool condition = true;
+            Console.WriteLine(condition);
+
+            string name = "Habibor Rahaman";
+            Console.WriteLine(name);
+
+            char character = 'H';
+            character = 'M';
+            Console.WriteLine(character);
+
+            const double PI = 3.1416;
+            Console.WriteLine(PI);
+        }
+    }
+}
+
