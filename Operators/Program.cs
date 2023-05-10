29048cbf8e5841933639c37b303a3facf8266293
habibor-rahaman1010 <habibor144369@gmail.com>
2023-05-10 23:12:06 +0600

    ok

---
+++ Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+            if (condition == true)
+            {
+                Console.WriteLine("Ok i'm going yet");
+            }
+            else
+            {
+                Console.WriteLine("i'm not go yet");
+            }
+        }
+    }
+}
