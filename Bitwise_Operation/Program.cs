0179f9249701e0704641a06bde4956e544e9f84a
habibor-rahaman1010 <habibor144369@gmail.com>
2023-10-01 20:52:54 +0600

    ok

---
+++ Bitwise_Operation/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace BitwiseOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int age = 23;
+            bool operation = age > 10 && age < 100;
+            Console.WriteLine(operation);
+
+            operation = age < 10 || age < 100;
+            Console.WriteLine(operation);
+
+            operation = age != 23;
+            Console.WriteLine(operation);
+
+            operation = age != 20;
+            Console.WriteLine(operation);
+
+            uint n1 = 0b_11001011;
+            Console.WriteLine(Convert.ToString(n1, toBase: 2));
+
+            //toggle operation om binary number....
+            uint n2 = 0b_0000_1111_0000_1111_0000_1111_0000_1100;
+            Console.WriteLine(Convert.ToString(n2, toBase: 2));
+            uint n3 = ~n2;
+            Console.WriteLine(Convert.ToString(n3, toBase: 2));
+
+            //left shift operator user in binary number...
+            uint b1 = 0b_0000_1111_0000_1111_0000_1111_0000_1100;
+            uint b2 = b1 << 4;
+            Console.WriteLine(Convert.ToString(b2, toBase: 2));
+
+            //right shift operator user in binary number...
+            uint b3 = 0b_0000_1111_0000_1111_0000_1111_0000_1100;
+            uint b4 = b3 >> 2;
+            Console.WriteLine(Convert.ToString(b4, toBase: 2));
+
+            // And operation in binary number...
+            uint a = 0b_0101_1010;
+            uint b = 0b_1100_0011;
+            uint c = a & b;
+            Console.WriteLine(Convert.ToString(c, toBase: 2));
+
+            // Or operation in binary number...
+            uint x = 0b_0101_1010;
+            uint y = 0b_1100_0011;
+            uint z = x | y;
+            Console.WriteLine(Convert.ToString(z, toBase: 2));
+
+            // Xor operation in binary number...
+            uint n = 0b_0101_1010;
+            uint m = 0b_1100_0011;
+            uint k = n ^ m;
+            Console.WriteLine(Convert.ToString(k, toBase: 2));
+
+            //This is my simple decimal to binary calculator...
+            while (true)
+            {
+                Console.WriteLine("Enter your decimal number: ");
+                string input = Console.ReadLine();
+
+                try
+                {
+                    long decimalValue = long.Parse(input);
+                    Console.WriteLine("Binary: {0}", Convert.ToString(decimalValue, toBase: 2));
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid decimal number.");
+                }
+            }
+        }
+    }
+}
---
+++ StringDataType/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace StringDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Habibor Rahaman";
+            string location = "Mohammodput - Dhaka";
+            Console.WriteLine("Hello {0}, you are from {1}", name, location);
+            Console.WriteLine("Hello {1}, you are from {0}", location, name);
+            Console.WriteLine($"Hello {name}, you are from {location}");
+
+            //string line = new string(Console.ReadLine());
+            string line2 = Console.ReadLine();
+            int x = Console.Read();
+            int y = Console.Read();
+
+            int data = Console.Read();
+            //Console.WriteLine((char)data);
+
+            ConsoleKeyInfo data2 = Console.ReadKey();
+            int z = Console.Read();
+            int v = Console.Read();
+
+            //Console.WriteLine();
+            //Console.WriteLine(data2.Key);
+            //Console.WriteLine(data2.KeyChar);
+
+            int[] nums = new int[3];
+            string inputLine = Console.ReadLine();
+            string[] partsLine = inputLine.Split(',', ' ');
+
+            nums[0] = int.Parse(partsLine[0]);
+            nums[1] = int.Parse(partsLine[1]);
+            nums[2] = int.Parse(partsLine[2]);
+        }
+    }
+}
